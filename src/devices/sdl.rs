d1ce642320f4d08cd3189bf1354dc67929601ea0
shady831213 <shady831213@126.com>
2020-05-18 23:28:11 -0700

    adding sdl

--- src/devices/sdl.rs
+++ src/devices/sdl.rs
@@ -13,20 +13,18 @@ use self::sdl2::surface::Surface;
 use self::sdl2::pixels::PixelFormatEnum;
 use self::sdl2::video::WindowContext;
 
-pub struct SDL<FB: FrameBuffer, I: DisplayInput> {
+pub struct SDL {
     context: Sdl,
     canvas: RefCell<WindowCanvas>,
-    texture_creator:TextureCreator<WindowContext>,
+    texture_creator: TextureCreator<WindowContext>,
     width: usize,
     height: usize,
-    fb: FB,
     key_pressed: RefCell<[bool; 256]>,
     quit: Box<dyn Fn() + 'static>,
-    input: I,
 }
 
-impl<FB: FrameBuffer, I: DisplayInput> SDL<FB, I> {
-    pub fn new<QF: Fn() + 'static>(title: &str, fb: FB, width: usize, height: usize, quit: QF, input: I) -> Result<SDL<FB, I>, String> {
+impl SDL {
+    pub fn new<QF: Fn() + 'static>(title: &str, width: usize, height: usize, quit: QF) -> Result<SDL, String> {
         let context = sdl2::init()?;
         let video_subsystem = context.video()?;
         let window = video_subsystem.window(title, width as u32, height as u32)
@@ -36,7 +34,7 @@ impl<FB: FrameBuffer, I: DisplayInput> SDL<FB, I> {
 
         let canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
         let texture_creator = canvas.texture_creator();
-        let cursor_data = vec![0;1];
+        let cursor_data = vec![0; 1];
         let cursor = Cursor::new(&cursor_data, &cursor_data, 8, 1, 0, 0)?;
         cursor.set();
         Ok(SDL {
@@ -45,58 +43,56 @@ impl<FB: FrameBuffer, I: DisplayInput> SDL<FB, I> {
             texture_creator,
             width,
             height,
-            fb,
             key_pressed: RefCell::new([false; 256]),
             quit: Box::new(quit),
-            input,
         })
     }
 
-    fn key_up(&self, code: &Keycode) {
+    fn key_up<I: DisplayInput>(&self, input: &I, code: &Keycode) {
         let idx = *code as i32 as usize;
         let mut key_pressed = self.key_pressed.borrow_mut();
         if (*key_pressed)[idx] {
             (*key_pressed)[idx] = false;
-            self.input.send_key_event(false, idx as u32)
+            input.send_key_event(false, idx as u32)
         }
     }
 
-    fn key_down(&self, code: &Keycode) {
+    fn key_down<I: DisplayInput>(&self, input: &I, code: &Keycode) {
         let idx = *code as i32 as usize;
         (*self.key_pressed.borrow_mut())[idx] = true;
-        self.input.send_key_event(true, idx as u32)
+        input.send_key_event(true, idx as u32)
     }
 
-    fn mouse_motion(&self, state: &MouseState, x: i32, y: i32, xrel: i32, yrel: i32) {
-        if self.input.mouse_absolute() {
-            self.input.send_mouse_event(self.mouse_x_abs(x), self.mouse_y_abs(y), 0, state.to_sdl_state())
+    fn mouse_motion<I: DisplayInput>(&self, input: &I, state: &MouseState, x: i32, y: i32, xrel: i32, yrel: i32) {
+        if input.mouse_absolute() {
+            input.send_mouse_event(self.mouse_x_abs(x), self.mouse_y_abs(y), 0, state.to_sdl_state())
         } else {
-            self.input.send_mouse_event(xrel, yrel, 0, state.to_sdl_state())
+            input.send_mouse_event(xrel, yrel, 0, state.to_sdl_state())
         }
     }
 
-    fn mouse_button_down(&self, x: i32, y: i32, sdl_btn: &MouseButton) {
+    fn mouse_button_down<I: DisplayInput>(&self, input: &I, x: i32, y: i32, sdl_btn: &MouseButton) {
         let btn = self.mouse_btn(sdl_btn);
-        if self.input.mouse_absolute() {
-            self.input.send_mouse_event(self.mouse_x_abs(x), self.mouse_y_abs(y), 0, btn)
+        if input.mouse_absolute() {
+            input.send_mouse_event(self.mouse_x_abs(x), self.mouse_y_abs(y), 0, btn)
         } else {
-            self.input.send_mouse_event(0, 0, 0, btn)
+            input.send_mouse_event(0, 0, 0, btn)
         }
     }
 
-    fn mouse_button_up(&self, x: i32, y: i32) {
-        if self.input.mouse_absolute() {
-            self.input.send_mouse_event(self.mouse_x_abs(x), self.mouse_y_abs(y), 0, 0)
+    fn mouse_button_up<I: DisplayInput>(&self, input: &I, x: i32, y: i32) {
+        if input.mouse_absolute() {
+            input.send_mouse_event(self.mouse_x_abs(x), self.mouse_y_abs(y), 0, 0)
         } else {
-            self.input.send_mouse_event(0, 0, 0, 0)
+            input.send_mouse_event(0, 0, 0, 0)
         }
     }
 
-    fn mouse_wheel(&self, x: i32, y: i32, dir: &MouseWheelDirection) {
-        if self.input.mouse_absolute() {
-            self.input.send_mouse_event(self.mouse_x_abs(x), self.mouse_y_abs(y), self.mouse_z(dir), 0)
+    fn mouse_wheel<I: DisplayInput>(&self, input: &I, x: i32, y: i32, dir: &MouseWheelDirection) {
+        if input.mouse_absolute() {
+            input.send_mouse_event(self.mouse_x_abs(x), self.mouse_y_abs(y), self.mouse_z(dir), 0)
         } else {
-            self.input.send_mouse_event(0, 0, self.mouse_z(dir), 0)
+            input.send_mouse_event(0, 0, self.mouse_z(dir), 0)
         }
     }
 
@@ -127,20 +123,20 @@ impl<FB: FrameBuffer, I: DisplayInput> SDL<FB, I> {
     }
 
 
-    pub fn refresh(&self) -> Result<(), String> {
-        self.fb.refresh(self)?;
+    pub fn refresh<FB: FrameBuffer, I: DisplayInput>(&self, fb: &FB, input: &I) -> Result<(), String> {
+        fb.refresh(self)?;
         let mut event_pump = self.context.event_pump()?;
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. } => {
                     (*&self.quit)()
                 }
-                Event::KeyDown { keycode: Some(ref code), .. } => { self.key_down(code) }
-                Event::KeyUp { keycode: Some(ref code), .. } => { self.key_up(code) }
-                Event::MouseMotion { mousestate: ref state, x,y, xrel, yrel, .. } => { self.mouse_motion(state, x, y, xrel, yrel) }
-                Event::MouseButtonDown { x,y, mouse_btn: ref btn, .. } => { self.mouse_button_down(x, y, btn) }
-                Event::MouseButtonUp { x,y, .. } => { self.mouse_button_up(x, y) }
-                Event::MouseWheel { x,y, direction: ref dir, .. } => { self.mouse_wheel(x, y, dir) }
+                Event::KeyDown { keycode: Some(ref code), .. } => { self.key_down(input, code) }
+                Event::KeyUp { keycode: Some(ref code), .. } => { self.key_up(input, code) }
+                Event::MouseMotion { mousestate: ref state, x, y, xrel, yrel, .. } => { self.mouse_motion(input, state, x, y, xrel, yrel) }
+                Event::MouseButtonDown { x, y, mouse_btn: ref btn, .. } => { self.mouse_button_down(input, x, y, btn) }
+                Event::MouseButtonUp { x, y, .. } => { self.mouse_button_up(input, x, y) }
+                Event::MouseWheel { x, y, direction: ref dir, .. } => { self.mouse_wheel(input, x, y, dir) }
                 _ => {}
             }
         }
@@ -148,9 +144,9 @@ impl<FB: FrameBuffer, I: DisplayInput> SDL<FB, I> {
     }
 }
 
-impl<FB: FrameBuffer, I: DisplayInput> Display for SDL<FB, I> {
-    fn draw(&self, data:&mut [u8], x: i32, y: i32, w: u32, h: u32) -> Result<(), String> {
-        let surface = Surface::from_data(data, w, h, w*h*4, PixelFormatEnum::RGB888)?;
+impl Display for SDL {
+    fn draw(&self, data: &mut [u8], x: i32, y: i32, w: u32, h: u32) -> Result<(), String> {
+        let surface = Surface::from_data(data, w, h, w * h * 4, PixelFormatEnum::RGB888)?;
         let texture = self.texture_creator.create_texture_from_surface(&surface)
             .map_err(|e| e.to_string())?;
         let mut canvas = self.canvas.borrow_mut();
