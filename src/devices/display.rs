ef58969fd0cac78080046ad605eb30c7d35fe360
shady831213 <shady831213@126.com>
2020-05-20 00:50:55 -0700

    adding sdl

--- src/devices/display.rs
+++ src/devices/display.rs
@@ -6,7 +6,7 @@ use std::cell::RefMut;
 
 pub trait FrameBuffer {
     fn refresh<DRAW: FnMut(i32, i32, u32, u32)->Result<(), String>>(&self, d: DRAW) -> Result<(), String>;
-    fn data(&self)->RefMut<'_, [u8]>;
+    fn data(&self)->RefMut<'_, Vec<u8>>;
     fn width(&self)->u32;
     fn height(&self)->u32;
     fn stride(&self)->u32;
